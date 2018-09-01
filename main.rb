a1d49236355f2b7adb7f7b782c21aa52c73ffc2f
Reka <rekapap28@gmail.com>
2018-09-01 02:05:06 +0100

    add comments

--- main.rb
+++ main.rb
@@ -8,27 +8,11 @@ class Game
     @user2 = User.new(user2)
   end
 
-  def icon
-    puts "#{@user1.choice.type.icon}  VS. #{@user2.choice.type.icon}"
-  end
-
-  def fightmaker
-    if @user1.choice.type.name == @user2.choice.type.name
-      puts "\nTIE!\n\n"
-      run
-    else 
-      winner = @user1
-      winner = @user2 if @user2.choice.type.win == @user1.choice.type.name
-      @winner = winner.name
-      puts "\nThe winner is...\n#{@winner}!\n\nWho win next? You decide!\n\n"
-    end
-  end
-
   def get_choice(player)
     choice = ''
     loop do
       puts "What is #{player.name}'s choice?"
-      choice = STDIN.noecho(&:gets).chomp.downcase #hides the input
+      choice = STDIN.noecho(&:gets).chomp.downcase # hides the input
       puts ''
       break if choice == 'rock' || choice == 'paper' || choice == 'scissor' 
       puts 'No such a move!'
@@ -36,19 +20,33 @@ class Game
     player.set_choice(choice)
   end
 
+  def icon
+    puts "#{@user1.choice.type.icon}  VS. #{@user2.choice.type.icon}"
+  end
+
+  def fightmaker
+    if @user1.choice.type.name == @user2.choice.type.name
+      puts "\nTIE!\n\n" # tie outcome string
+      run
+    else 
+      winner = @user1
+      winner = @user2 if @user2.choice.type.win == @user1.choice.type.name
+      @winner = winner.name
+      puts "\nThe winner is...\n#{@winner}!\n\nWho win next? You decide!\n\n" # displaying winner + outro string
+    end
+  end
+
   def run
-    puts "4!\n3!\n1!\nOFFBLAST!\n"
-    
+    puts "4!\n3!\n1!\nOFFBLAST!\n" # countdown string
     get_choice(@user1)
     get_choice(@user2)
-
-    icon
-    fightmaker
+    icon # display icons
+    fightmaker # select and display winner
   end
 
 end
 
 if __FILE__ == $PROGRAM_NAME
-  Main = Game.new('Jane', 'John')
+  Main = Game.new('Jane', 'John') # Player's names
   Main.run
 end
